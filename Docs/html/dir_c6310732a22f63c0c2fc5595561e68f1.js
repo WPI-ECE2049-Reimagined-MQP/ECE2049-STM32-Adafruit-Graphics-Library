372498ed272758bea1dbf8e96d6ee71b3096ba5a
Kyle Schmottlach <kyleschmottlach@gmail.com>
2026-03-11 09:45:30 +0100

    Renaming Docs to docs

--- Docs/html/dir_c6310732a22f63c0c2fc5595561e68f1.js
+++
@@ -1,4 +0,0 @@
-var dir_c6310732a22f63c0c2fc5595561e68f1 =
-[
-    [ "Inc", "dir_e2489e887f17afa3cbc07a4ec152cdd2.html", "dir_e2489e887f17afa3cbc07a4ec152cdd2" ]
-];
--- Docs/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['st7735_5fhandle_0',['ST7735_Handle',['../d7/d3c/_adafruit___s_t7735___a_p_i_8h.html#a07d1198edab438920f1da873cdbb4e7f',1,'Adafruit_ST7735_API.h']]]
-];
